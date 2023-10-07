c9f04af86abc3469bddfab9df83d017c99d24bde
mai-reaska <2820.mai.reaksa@rupp.edu.kh>
2023-10-07 10:37:27 +0700

    first commit setup project

---
+++ Python/introduction.py
@@ -0,0 +1,29 @@
+# Basic print in Python
+print('Hello')
+
+# Varaible in Pthon 
+varlInt = 12
+valString = "Hello World"
+valFloat = 34.11
+print(valString)
+print(varlInt)
+print(valFloat)
+
+# Conditional in Python
+i = 10
+if i==10:
+    print(i)
+
+if i>=0:
+    print(i)
+else:
+    print('No Print')
+
+if i!=1 & i<10:
+    s = i =+ 2
+    print(s)
+elif i == 0 | i == 1:
+    pass
+else:
+    print('Thank!') 
+
