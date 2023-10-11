3ff0cabfa548ddd372b175644136d6d02e61f289
mai-reaska <2820.mai.reaksa@rupp.edu.kh>
2023-10-11 23:25:39 +0700

    intro function python-v1

--- Python/function_python.py
+++ Python/function_python.py
@@ -1,2 +1,40 @@
+# Simple function using Pass keyworld
 def hello_worl():
     pass
+
+# Function don't have Parameter
+def fun_noParam():
+    print("Hello Function No Paramter")
+
+# Function Have Paramter Number
+def fun_haveParam(a, b):
+    c = a + b
+    print(c)
+
+# Function Have Paramter Number
+def fun_strParam(f_name, l_name):
+    my_name = f_name + l_name
+    print("My name is ", my_name)
+
+# Function Pass param by *arge
+def fun_argeParam(*name):
+    print("Pass by ", name)
+
+# Fuction Pass by **kwargs
+def fun_kwargsParam(**data):
+    print(data)
+
+# Function Use Variabel 
+def fun_variabel(x, y):
+    return x + y
+
+# Call function have param pass value
+fun_haveParam(10, 20)
+fun_strParam('mai','reaksa')
+fun_argeParam('reaksa','lika','sokhim','daly')
+fun_kwargsParam(f_name="reaksa", l_name="mai", age=21, email="reaksamai@rupp.edu.kh")
+
+# Call fun by Varibel
+call_fun = fun_variabel(1, 3)
+print(call_fun)
+
